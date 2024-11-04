6fb047040728c0b14951a48d9156f9eb38433751
Velma Atieno <atienovelma1@gmail.com>
2024-11-04 11:19:57 +0300

    weather app API intergration

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
+function refreshWeather(response){
+   let temperatureElement = document.querySelector("#weatherappTemperature");
+   console.log(temperatureElement)
+   let temperature= response.data.temperature.current;
+   let cityElement = document.querySelector("#city");
+
+   
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML= Math.round(temperature);
+
+}
+
+
+function searchCity (city){
+    let apiKey = "9094e1e8bc4be419b3a3btd1e9oa78f2";
+    let apiUrl =
+      `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey} `;
+      
+      axios.get(apiUrl).then(refreshWeather)
+
+}
+
 //search engine functionality
 function SearchSubmitButton(event){
     event.preventDefault();
     let searchInput = document.querySelector("#searchFormInput");
 
-    let cityElement = document.querySelector("#city");
-
-    cityElement.innerHTML= searchInput.value;
+    searchCity(searchInput.value);
 };
 
 let weatherSearchForm = document.querySelector("#searchForm");
 
 weatherSearchForm.addEventListener("submit", SearchSubmitButton);
+
+searchCity("Nairobi")
